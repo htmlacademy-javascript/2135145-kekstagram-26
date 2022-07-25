5c691aa26f65397c13a43eb68cfe77c96c0f587c
irinastyazhkina <irastyazhkina@gmail.com>
2022-07-26 00:06:47 +0300

    add scale and photo effects

--- js/form.js
+++ js/form.js
@@ -9,6 +9,16 @@ const hashTagInput = form.querySelector('.text__hashtags');
 const hashtagsInput = form.querySelector('.text__hashtags');
 const commentsInput = form.querySelector('.text__description');
 
+const scaleContainer = document.querySelector('.scale');
+const scaleSmallerButton = scaleContainer.querySelector('.scale__control--smaller');
+const scaleBiggerButton = scaleContainer.querySelector('.scale__control--bigger');
+const scaleInput = scaleContainer.querySelector('.scale__control--value');
+const imagePreview = document.querySelector('.img-upload__preview img');
+
+const SCALE_STEP = 25;
+const MIN_SCALE_VALUE = 25;
+const MAX_SCALE_VALUE = 100;
+
 
 const onKeyPressed = (evt) => {
   if (evt.target.tagName === 'INPUT' || evt.target.tagName === 'TEXTAREA') {
@@ -40,6 +50,27 @@ uploadFileInput.addEventListener('change', () => {
 
 closeButton.addEventListener('click', () => {
   closeForm();
+});
+
+const zoom = (value) => {
+  scaleInput.value = `${value}%`;
+  imagePreview.style.transform = `scale(${value / 100})`;
+};
+
+scaleBiggerButton.addEventListener('click', () => {
+  let value = Number(scaleInput.value.slice(0, -1));
+  if (value < MAX_SCALE_VALUE) {
+    value += SCALE_STEP;
+    zoom(value);
+  }
+});
+
+scaleSmallerButton.addEventListener('click', () => {
+  let value = Number(scaleInput.value.slice(0, -1));
+  if (value > MIN_SCALE_VALUE) {
+    value -= SCALE_STEP;
+    zoom(value);
+  }
 });
 
 const pristine = new Pristine(form,
