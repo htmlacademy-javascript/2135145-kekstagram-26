181daafe9ab1553b9f7fea5d983a26f7fb09f64c
irinastyazhkina <irastyazhkina@gmail.com>
2022-07-27 21:04:50 +0300

    add upload photo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 import {renderThumbnails} from './thumbnails.js';
 import {getPhotos} from './api.js';
 import {debounce, showAlert} from './util.js';
-import './form.js';
-import './image-effects.js';
 import {setSwitchFiltersView, showFilters} from './filters.js';
+import {hideSlider} from './image-effects.js';
+import './form.js';
 
+hideSlider();
 getPhotos((photos) => {
   renderThumbnails(photos);
   setSwitchFiltersView(photos, debounce(renderThumbnails));
